545cbb29a2fa1177c82215ba12baef59d4fc7910
myname <myname@example.com>
2021-02-19 21:03:24 +0800

    按照job类型调度

--- background_job/Scheduler.py
+++ background_job/Scheduler.py
@@ -18,37 +18,66 @@ class Scheduler(threading.Thread):
         self.setDaemon(False)
         self.queue = queue
         # load from db
-        self.job_list = self.__load_jobs()
+        self.__load_jobs()
         self.timer = sched.scheduler(time.time, time.sleep)
 
-    @staticmethod
-    def __load_jobs():
+    def __load_jobs(self):
         jobs = DjangoJob.objects.all()
-        return jobs
+        self.job_list = jobs
 
     def run(self):
         if len(self.job_list)<=0:
             logger.info("no task to schedule")
         else:
             for job in self.job_list:
-                self.__lunch_job(job)
+                if job.trigger_type in ['cron', 'interval']:
+                    self.__lunch_periodical_job(job)
+                elif job.trigger_type=='once':
+                    self.__lunch_once_job(job)
+                else:
+                    self.__lunch_once_boot_job(job)
         while True:
             self.timer.run(blocking=True)
-            logger.warning("调度无任务，暂时退出")
+            logger.warning("调度无任务，暂时休眠")
             time.sleep(1)
 
-    def __lunch_job(self, job):
+    def __lunch_periodical_job(self, job):
         seconds_to_wait, _ = job.next_run_time()
-        if seconds_to_wait<0 and abs(seconds_to_wait)<=job.misfire_grace_time:
+        if seconds_to_wait<0 and abs(seconds_to_wait)<=job.misfire_grace_time: # 是否在指定时间容错范围内可以执行
             seconds_to_wait = 0
         elif seconds_to_wait<0:
-            # TODO 根据记录决定是否记录
+            # TODO 根据loglevel决定是否记录
             logger.info("task [%s] missed! (delay, misfire_grace_time)=(%s, %s)", job.job_function, seconds_to_wait, job.misfire_grace_time)
             return
         logger.info("task [%s] will invoke after [%f] seconds later", job.job_function, seconds_to_wait)
         self.timer.enter(seconds_to_wait, 0, self.__fire_job, argument=(job, ))
 
-    def __fire_job(self, job):
+    def __lunch_once_job(self, job):
+        seconds_to_wait, _ = job.next_run_time()
+        if seconds_to_wait < 0 and abs(seconds_to_wait) <= job.misfire_grace_time:  # 是否在指定时间容错范围内可以执行
+            seconds_to_wait = 0
+        elif seconds_to_wait < 0:
+            # TODO 根据loglevel决定是否记录
+            logger.info("task [%s] missed! (delay, misfire_grace_time)=(%s, %s)", job.job_function, seconds_to_wait,
+                        job.misfire_grace_time)
+            return
+        logger.info("task [%s] will invoke after [%f] seconds later", job.job_function, seconds_to_wait)
+        self.timer.enter(seconds_to_wait, 0, self.__fire_once_job, argument=(job,))
+
+    def __fire_once_job(self, job):
+        self.__lunch_once_boot_job(job)
+
+    def __lunch_once_boot_job(self, job):
+        try:
+            self.queue.put_nowait(job)
+        except queue.Full as e:
+            logger.exception(e)
+            # TODO log job missed!
+        except Exception as e:
+            logger.exception(e)
+            # TODO log job missed!
+
+    def __fire_job(self, job:DjangoJob):
         try:
             self.queue.put_nowait(job)
         except queue.Full as e:
--- example/jobs.py
+++ example/jobs.py
@@ -1,20 +1,20 @@
 from background_job.job import cron_job, delayed_job, interval_job, once_job, boot_job
 
 
-# @cron_job(name="my_func3", cron="*/1 * * * *")
-# def my_func3():
-#     print("my_func3()")
-#     return "this is func3"
-#
-#
-# @cron_job(name="my_func", cron="*/2 * * * *", args=('X parameter',), kwargs={"name":"Jin", "age":23})
-# def my_func(x, name="Jos", age=20):
-#     print(f"my_func(x={x}, name={name}, age={age})")
-#
-#
-# @boot_job(name="my_func2", )
-# def my_func2():
-#     print("my_func2()")
+@cron_job(name="my_func3", cron="*/1 * * * *")
+def my_func3():
+    print("my_func3()")
+    return "this is func3"
+
+
+@cron_job(name="my_func", cron="*/2 * * * *", args=('X parameter',), kwargs={"name":"Jin", "age":23})
+def my_func(x, name="Jos", age=20):
+    print(f"my_func(x={x}, name={name}, age={age})")
+
+
+@boot_job(name="my_func2", )
+def my_func2():
+    print("my_func2()")
 
 
 @interval_job(name="间隔执行", seconds=5, args=('NAME',"VALUE"))
@@ -22,15 +22,15 @@ def interval_func(name, value):
     print(f"interval_func({name}, {value})")
     return "interval_func ************************"
 
-#
-# @once_job(name="once_job只运行一次", run_at="2021-02-16 23:39:00", args=("NAME",), kwargs={"key":"KEY"})
-# def run_once_func(name, value=33, key="xyz"):
-#     print(f"run_once_func({name}, {value}, {key})")
-#
-#
-# @delayed_job(name="测试用delayed_job", retry=-1, description="只是用在测试上")
-# def delayed_job_func(name, value):
-#     print(f"{delayed_job_func}({name}, {value})")
+
+@once_job(name="once_job只运行一次", run_at="2021-02-16 23:39:00", args=("NAME",), kwargs={"key":"KEY"})
+def run_once_func(name, value=33, key="xyz"):
+    print(f"run_once_func({name}, {value}, {key})")
+
+
+@delayed_job(name="测试用delayed_job", retry=-1, description="只是用在测试上")
+def delayed_job_func(name, value):
+    print(f"{delayed_job_func}({name}, {value})")
 
 
 def common_func(name, value):
